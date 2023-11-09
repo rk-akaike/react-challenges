623ef6b497c98b556a027c50dfd2dec6ca4263dc
rk-akaike <ramakrishna@akaiketech.com>
2023-11-09 15:22:04 +0530

    focus-the-search-field-with-separate-components

---
+++ Manipulating the DOM with Refs/focus-the-search-field-with-separate-components/src/App.tsx
@@ -0,0 +1,13 @@
+import SearchButton from "./SearchButton";
+import SearchInput from "./SearchInput";
+
+export default function Page() {
+  return (
+    <>
+      <nav>
+        <SearchButton />
+      </nav>
+      <SearchInput />
+    </>
+  );
+}
