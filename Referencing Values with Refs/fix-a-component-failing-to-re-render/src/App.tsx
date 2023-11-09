53f7dcf10e00f9bec2daf35fb3b1544a0558175d
rk-akaike <ramakrishna@akaiketech.com>
2023-11-09 15:03:30 +0530

    fix-a-component-failing-to-re-render

---
+++ Referencing Values with Refs/fix-a-component-failing-to-re-render/src/App.tsx
@@ -0,0 +1,15 @@
+import { useRef } from "react";
+
+export default function Toggle() {
+  const isOnRef = useRef(false);
+
+  return (
+    <button
+      onClick={() => {
+        isOnRef.current = !isOnRef.current;
+      }}
+    >
+      {isOnRef.current ? "On" : "Off"}
+    </button>
+  );
+}
